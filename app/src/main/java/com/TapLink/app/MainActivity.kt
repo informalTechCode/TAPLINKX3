a5443141a960c8d1432d6663112f74f2cc5107ba
informalTechCode <informaltechyt@gmail.com>
2025-11-18 22:48:13 -0700

    Set AR dashboard as default home and polish layout

--- app/src/main/java/com/TapLink/app/MainActivity.kt
+++ app/src/main/java/com/TapLink/app/MainActivity.kt
@@ -3131,16 +3131,18 @@ class MainActivity : AppCompatActivity(),
             val lastUrl = prefs.getString(keyLastUrl, null)
             Log.d("WebViewDebug", "Last saved URL: $lastUrl")
 
+            val defaultDashboardUrl = "file:///android_asset/AR_Dashboard_Landscape_Sidebar.html"
+
             if (lastUrl != null && !lastUrl.startsWith("about:blank")) {
                 Log.d("WebViewDebug", "Loading saved URL: $lastUrl")
                 webView.loadUrl(lastUrl)
             } else {
-                Log.d("WebViewDebug", "No valid saved URL, loading default page")
-                webView.loadUrl("https://www.google.com")
+                Log.d("WebViewDebug", "No valid saved URL, loading default AR dashboard")
+                webView.loadUrl(defaultDashboardUrl)
             }
         } catch (e: Exception) {
             Log.e("WebViewDebug", "Error restoring session", e)
-            webView.loadUrl("https://www.google.com")
+            webView.loadUrl("file:///android_asset/AR_Dashboard_Landscape_Sidebar.html")
         }
 
         setupMediaWebView()
